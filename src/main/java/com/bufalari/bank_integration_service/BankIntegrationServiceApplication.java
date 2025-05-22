d4ce52620666618438f0999bceb862ee330f030c
Fabio Bufalari <bufalari.fabio@gmail.com>
2025-05-21 22:56:15 -0300

    initial commit

---
+++ src/main/java/com/bufalari/bank_integration_service/BankIntegrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bufalari.bank_integration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankIntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankIntegrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bufalari/bank_integration_service/BankIntegrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bufalari.bank_integration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankIntegrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
